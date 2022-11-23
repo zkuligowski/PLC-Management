9d210654aa0519da857ac685d2faae4040f90016
zkuligowski <62381241+zkuligowski@users.noreply.github.com>
2022-11-23 19:02:32 +0100

    Implement DotNetSiemensPLCToolBoxLibrary

--- PlcApp/PlcApp/Classes/ConnectionPLC.cs
+++ PlcApp/PlcApp/Classes/ConnectionPLC.cs
@@ -2,21 +2,76 @@
 
 namespace PlcApp.Classes
 {
+    using DotNetSiemensPLCToolBoxLibrary.Communication;
     using S7.Net;
+    using System.Net;
+    using System.Windows.Controls.Primitives;
+    using System;
+    using DotNetSiemensPLCToolBoxLibrary.DataTypes;
+    using System.Diagnostics;
+    using System.Collections.Generic;
 
     public class ConnectionPLC
     {
         private readonly Plc? plc;
+        private PLCConnection _plcConnection;
 
         public ConnectionPLC(string ipAdress)
         {
-            this.plc = new Plc(CpuType.S71200, ipAdress, 0, 1);
-            this.plc.Open();
+            var cfg = new PLCConnectionConfiguration
+            {
+                CpuIP = ipAdress,
+                Port = 102,
+                CpuRack = 0,
+                CpuSlot = 1,
+                TimeoutIPConnect = TimeSpan.FromSeconds(5),
+                Timeout = TimeSpan.FromSeconds(5),
+                ConnectionType = LibNodaveConnectionTypes.ISO_over_TCP,
+                ConnectionName = "S7_Connection",
+                Routing = false,
+                RoutingDestinationRack = 0,
+                RoutingDestinationSlot = 0,
+                RoutingDestination = string.Empty,
+            };
+
+            cfg.SaveConfiguration();
+
+            this._plcConnection = new PLCConnection(cfg);
+
+            this._plcConnection.Connect();
+
+            var testconnection = this._plcConnection.Connected;
+
+            var bool1 = new PLCTag("DB1.DBX0.0", TagDataType.Bool) { Value = 0 };
+            var bool2 = new PLCTag("DB1.DBX0.0", TagDataType.Bool) { Value = 0 };
+            var db1IntVariable = new PLCTag("DB1.DBW2.0", TagDataType.Int) { Value = 51 };
+            var db1RealVariable = new PLCTag("DB1.DBD4.0", TagDataType.Float) { Value = 25.40 };
+            var db1DintVariable = new PLCTag("DB1.DBD8.0", TagDataType.Dint) { Value = 123451 };
+            var db1DWordVariable = new PLCTag("DB1.DBD12.0", TagDataType.Dword) { Value = 123451 };
+            var db1WordVariable = new PLCTag("DB1.DBW16.0", TagDataType.Word) { Value = 12341 };
+
+            this._plcConnection.WriteValue(bool1);
+            this._plcConnection.WriteValue(bool2);
+            this._plcConnection.WriteValue(db1IntVariable);
+            this._plcConnection.WriteValue(db1RealVariable);
+            this._plcConnection.WriteValue(db1DintVariable);
+            this._plcConnection.WriteValue(db1DWordVariable);
+            this._plcConnection.WriteValue(db1WordVariable);
+
+            this._plcConnection.ReadValue(bool1);
+            this._plcConnection.ReadValue(bool2);
+            this._plcConnection.ReadValue(db1IntVariable);
+            this._plcConnection.ReadValue(db1RealVariable);
+            this._plcConnection.ReadValue(db1DintVariable);
+            this._plcConnection.ReadValue(db1DWordVariable);
+            this._plcConnection.ReadValue(db1WordVariable);
+            var test = 0;
+
         }
 
         public void DisconnectPLC()
         {
-            this.plc.Close();
+            this._plcConnection.Disconnect();
         }
 
         public Db1 ReadSingleVariables()
--- PlcApp/PlcApp/obj/Debug/net6.0-windows/Views/LoginWindow.g.i.cs
+++ PlcApp/PlcApp/obj/Debug/net6.0-windows/Views/LoginWindow.g.i.cs
@@ -101,7 +101,7 @@ namespace PlcApp.Views {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/PlcApp;V1.0.0.0;component/views/loginwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/PlcApp;component/views/loginwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\LoginWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
